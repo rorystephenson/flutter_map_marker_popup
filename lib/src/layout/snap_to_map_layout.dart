fd1af172fb200632d6d7ac1df681a630f94d0838
Pablo Jiménez Pascual <pablo@jimpas.me>
2022-06-08 11:57:40 +0200

    Replace ugly cast with a local variable

--- lib/src/layout/snap_to_map_layout.dart
+++ lib/src/layout/snap_to_map_layout.dart
@@ -44,10 +44,8 @@ abstract class SnapToMapLayout {
   }
 
   static CustomPoint<num> _sizeChangeDueToRotation(MapState mapState) {
-    // Ugly cast needed because mapState.originalSize is just a CustomPoint?
-    // instead of a CustomPoint<double>? and the type system gets confused.
-    return mapState.size -
-        ((mapState.originalSize ?? mapState.size) as CustomPoint<double>);
+    final CustomPoint<num> size = mapState.size;
+    return size - (mapState.originalSize ?? mapState.size);
   }
 
   static PopupLayout _layoutWith({
